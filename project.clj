1731e76808b23af525b7ec286dc1158d7ba427cc
Chris Dean <ctdean@kyluka.com>
2010-02-28 16:02:11 -0800

    Use lein for all dep management

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.ctdean/clojure-tokyocabinet "0.4.0"
+(defproject org.clojars.ctdean/clojure-tokyocabinet "0.5.1"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.1.0"]
                  [org.clojars.ctdean/tokyocabinet "1.22.0"]
                  [tokyotyrant "0.8.1"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]
